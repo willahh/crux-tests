088ad81d33439409d57db6d63089c0b5933c5f47
wravel <wravel@electre.com>
2020-12-18 22:41:35 +0100

    Initial commit

--- tutorials/earth.clj
+++ tutorials/earth.clj
@@ -1,5 +1,17 @@
 (ns earth
   (:require [crux.api :as crux]))
 
-
 (def node (crux/start-node {}))
+
+(def manifest
+  {:crux.db/id :manifest
+   :pilot-name "Johanna"
+   :id/rocket "SB002-sol"
+   :id/employee "22910x2"
+   :badges "SETUP"
+   :cargo ["stereo" "gold fish" "slippers" "secret note"]})
+
+(crux/submit-tx node [[:crux.tx/put manifest]])
+
+(crux/entity-history (crux/db node) :manifest :asc)
+
